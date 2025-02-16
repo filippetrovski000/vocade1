9118ffef6ce1d91b7d9e353520aa255e63db07f7
Filip <filip@example.com>
2025-02-17 00:46:35 +0100

    [WIN-9] fix: prevent multiple instances of the app from running

--- vocade/src-tauri/src/main.rs
+++ vocade/src-tauri/src/main.rs
@@ -1,6 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use std::process;
+
 fn main() {
-    app_lib::run();
+    tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle();
+
+            // Prevent multiple instances
+            if let Err(_) = app_handle.try_global_single_instance() {
+                println!("Another instance of Vocade is already running. Exiting...");
+                process::exit(1);
+            }
+
+            Ok(())
+        })
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running Vocade");
 }
